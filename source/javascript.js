cf7e7ceb2117adb2c8025ad9f741a1306963a79b
JaniceAtHaverford <54867369+JaniceAtHaverford@users.noreply.github.com>
2021-02-03 02:11:11 -0500

    Update javascript.js

--- source/javascript.js
+++ source/javascript.js
@@ -1,15 +1,15 @@
+  
 var sents = ['Thrift clothes online',
             'Buy ethical cosmetics',
             'Donate clothes in Philly',
             'DIY to transform my clothes']
 var links = ["https://www.thegoodtrade.com/features/shopping-used-clothing-online",
-            "https://www.sustainablejungle.com/best-of-sustainable-beauty/best-cruelty-free-vegan-sustainable-makeup-brands-uk-2/",
-            "https://kensingtonvoice.com/en/here-are-7-places-you-can-donate-clothes-to-in-philly/",
-            "https://cutediyprojects.com/beauty-style/20-stylish-simple-diy-clothes-revamp-wardrobe/"]
-
+            "https://www.sustainablejungle.com/best-of-sustainable-beauty/best-cruelty-free-vegan-sustainable-makeup-brands-uk-2",
+            "https://kensingtonvoice.com/en/here-are-7-places-you-can-donate-clothes-to-in-philly",
+            "https://cutediyprojects.com/beauty-style/20-stylish-simple-diy-clothes-revamp-wardrobe"]
 function newQuote(){
 var randNum = Math.floor(Math.random() * sents.length);
-   
+
 var link = document.getElementById("myLink");
            // window.open(link.href,'_blank');
             
