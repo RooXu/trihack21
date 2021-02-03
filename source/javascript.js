d095116374ad650e59e1c6e03bcfeb7d5333537d
RooXu <56663690+RooXu@users.noreply.github.com>
2021-02-02 20:31:33 -0800

    Update javascript.js

--- source/javascript.js
+++ source/javascript.js
@@ -1,1 +1,12 @@
+var arrr = ["Google",
+              "Yahoo",
+              "Bing",
+              "Duckduckgo"][
+              "google.com",
+              "yahoo.com",
+              "bing.com",
+              "Duckduckgo"];
+var randomNumber = Math.floor(Math.random() * arrr.length);
 
+document.getElementById("suggestion_display").innerHTML = arrr[randomNumber][randomNumber];
+document.getElementById("suggestion_link").innerHTML = arrr[randomNumber][randomNumber];
