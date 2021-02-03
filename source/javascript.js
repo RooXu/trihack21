ab4a0a4337d250ec385de0c787ac00a14939a3b4
RooXu <56663690+RooXu@users.noreply.github.com>
2021-02-02 21:49:32 -0800

    Update javascript.js

--- source/javascript.js
+++ source/javascript.js
@@ -1,14 +1,10 @@
-var arrr = ['Googl',
+var arrr = ['Google',
             'Yahoo',
             'Bing',
-            'Duckduckgo'][
-            'google.com',
-            'yahoo.com',
-            'bing.com',
             'Duckduckgo']
 function newQuote(){
-var randomNumber = Math.floor(Math.random() * arrr[0].length);
+var randomNumber = Math.floor(Math.random() * arrr.length);
 
-document.getElementById("suggestion_display").innerHTML = arrr[0][randomNumber];
+document.getElementById("suggestion_display").innerHTML = arrr[randomNumber];
 
 }
